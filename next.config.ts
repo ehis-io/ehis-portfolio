116e64fdae8aac158ac44e8e1b342a7e2c649833
Ehis Fidelis <meelisfidelis@gmail.com>
2025-06-01 18:55:19 +0100

    redirection

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // <=== enables static exports
-  reactStrictMode: true,
+  output: 'export',
+  basePath: '/your-repo-name',
+  trailingSlash: true,
+  images: { unoptimized: true },
 }
 
 module.exports = nextConfig
