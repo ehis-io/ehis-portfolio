2eab15ba86d4912ca910dcae147c14f1291da221
Ehis Fidelis <meelisfidelis@gmail.com>
2025-09-05 19:09:48 +0100

    fix layout'

--- app/hooks/ThemeInitializer.tsx
+++ app/hooks/ThemeInitializer.tsx
@@ -1,22 +1,22 @@
-"use client";
+'use client';
 
-import { useEffect } from "react";
+import { useEffect } from 'react';
 
 export default function ThemeInitializer() {
   useEffect(() => {
     const applyTheme = () => {
       const prefersDark = window.matchMedia(
-        "(prefers-color-scheme: dark)"
+        '(prefers-color-scheme: dark)'
       ).matches;
-      document.documentElement.classList.remove("dark", "light");
-      document.documentElement.classList.add(prefersDark ? "dark" : "light");
+      document.documentElement.classList.remove('dark', 'light');
+      document.documentElement.classList.add(prefersDark ? 'dark' : 'light');
     };
 
-    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+    const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
     applyTheme(); // Initial call
-    mediaQuery.addEventListener("change", applyTheme); // Listen to changes
+    mediaQuery.addEventListener('change', applyTheme); // Listen to changes
 
-    return () => mediaQuery.removeEventListener("change", applyTheme);
+    return () => mediaQuery.removeEventListener('change', applyTheme);
   }, []);
 
   return null;
